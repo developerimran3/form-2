6334ea0db4165ae435ea45b780a5e2349eaf859d
developerimran <developerimran.in@gmail.com>
2024-09-16 12:54:43 +0600

    all done

--- autoload.php
+++ autoload.php
@@ -1,7 +1,13 @@
 <?php
-if (file_exists(__DIR__ . '/app/function.php')) {
-    require_once __DIR__ . '/app/function.php';
+if (file_exists(__DIR__ . '/app/functions.php')) {
+    require_once __DIR__ . '/app/functions.php';
 }
 if (file_exists(__DIR__ . '/app/data.php')) {
     require_once __DIR__ . '/app/data.php';
 }
+// if (file_exists(__DIR__ . '/app/main.php')) {
+//     require_once __DIR__ . '/app/main.php';
+// }
+// if (file_exists(__DIR__ . '/app/models.php')) {
+//     require_once __DIR__ . '/app/models.php';
+// }
--- index.php
+++ index.php
@@ -19,6 +19,26 @@ if (file_exists(__DIR__ . "/autoload.php")) {
 
 <body>
 
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        //get form valus
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $profile = $_FILES['profile'];
+
+        if (empty($name) || empty($phone)) {
+            $msg = createAlert("All fields are requerd");
+        } else {
+            $msg = createAlert("Data Submit", "success");
+            reset_form();
+        }
+    }
+
+
+
+
+    ?>
+
     <div class="container my-5">
         <div class="row justify-content-center my-5">
             <div class="col-md-5 my-5">
@@ -27,24 +47,25 @@ if (file_exists(__DIR__ . "/autoload.php")) {
                         <h2 class="card-title"> Uplode Your File</h2>
                     </div>
                     <div class="card-body">
-                        <form action="">
+                        <?php echo $msg ?? ''; ?>
+                        <form action="" method="POST" enctype="multipart/form-data">
                             <div class="my-3">
                                 <label for="">Name</label>
-                                <input type="text" class="form-control" name="name">
+                                <input type="text" class="form-control" name="name" value="<?php echo old("name"); ?>">
                             </div>
                             <div class="my-3">
                                 <label for="">Phone</label>
-                                <input type="text" class="form-control" name="phone">
+                                <input type="text" class="form-control" name="phone" value="<?php echo old("phone"); ?>">
                             </div>
                             <div class="my-3">
                                 <label for="">Profile Photo</label>
                                 <label class="uploaded">
-                                    <input type="file" id="profile-photo" class="form-control">
+                                    <input type="file" id="profile-photo" name="profile" class="form-control">
                                     <img id="profile-photo-icon" src="https://www.freeiconspng.com/uploads/pictures-icon-22.gif" alt="">
                                 </label>
                                 <div id="profile-photo-preview" class="preview-image">
                                     <img src="" alt="">
-                                    <button id="profile-photo-close" class=" fa fa-times"></button>
+                                    <button type="button" id="profile-photo-close" class=" fa fa-times"></button>
                                 </div>
                             </div>
 
@@ -60,16 +81,24 @@ if (file_exists(__DIR__ . "/autoload.php")) {
     </div>
     <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        const profilePhoto = document.getElementById("profile-photo");
-        const profilePhotoPreview = document.getElementById("profile-photo-preview");
+        const ProfilePhoto = document.getElementById('profile-photo');
+        const ProfilePhotoPreview = document.getElementById('profile-photo-preview');
+        const ProfilePhotoIcon = document.getElementById('profile-photo-icon');
+        const ProfilePhotoClose = document.getElementById('profile-photo-close');
 
 
-        profilePhoto.onchange = (event) => {
-
+        ProfilePhoto.onchange = (event) => {
             const imageURL = URL.createObjectURL(event.target.files[0]);
-            profilePhotoPreview.setAttribute('src',
-                imageURL);
 
+            ProfilePhotoPreview.children[0].setAttribute('src', imageURL);
+            ProfilePhotoIcon.style.display = 'none';
+            ProfilePhotoClose.style.display = "block";
+
+        };
+        ProfilePhotoClose.onclick = (event) => {
+            ProfilePhotoPreview.children[0].setAttribute('src', "");
+            ProfilePhotoIcon.style.display = 'block';
+            ProfilePhotoClose.style.display = "none";
         };
     </script>
 </body>
